742d9739a9c6fa0fd9e90f3e2e676fd19a88e9c4
Duke_of_Karamel <dukek@localhost.localdomain>
2020-05-21 21:02:38 +0200

    include comments

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 
 #include <iostream>
-#include <GL/glew.h>
+#include <GL/glew.h> // OpenGL API
+#include <GLFW/glfw3.h> // OpenGL toolkit for |window| managment and |input| managment
 
 int main(int argc, char* argv[])
 {
+    if (0) return 1;
 
     return 0;
 }
